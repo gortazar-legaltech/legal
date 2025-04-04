5ec88e79fabc361905489d2cef2b3bc070febaff
gortazar-legaltech <gortazar-legaltech@atlantyde.eu>
2025-04-04 21:48:36 +0200

    sadfasdf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: 'https://gortazar-legaltech.github.io/legal/',
-  integrations: [sitemap()],
-  outDir: './dist',
   base: '/legal/',
+  outDir: './dist',
+  integrations: [sitemap()],
 });
