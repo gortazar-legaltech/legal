f6b9efadbc6bc4ef604c3d6a018b4ff177b605ef
silvaglez <gazaping@hotmail.com>
2025-04-07 12:01:26 +0200

    wip monday

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: 'https://gortazar-legaltech.github.io/legal/',
+  site: 'https://gortazar-legaltech.github.io/',
   base: '/legal/',
   outDir: './dist',
   integrations: [sitemap()],
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en', 'pt', 'it'],
+  },
 });
